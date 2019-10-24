efd022443c1d1dbcbcab40a836b17ddd5e45ae79
Toleckk <anatolich1995@gmail.com>
2019-10-24 21:45:19 +0300

    Button added

---
+++ src/components/Info/Authorization/Button.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export default styled.button`
+    align-self: center;
+    padding: 10px 12px;
+    border-radius: 10px;
+    border: 1.5px solid rgb(2, 132, 132);
+    background: transparent;
+    color: rgb(202, 202, 220);
+    cursor: pointer;
+    font-size: medium;
+    user-select: none;
+    
+    transition: all 100ms;
+    
+    &:hover {
+        border-color: rgb(2, 132, 132);
+        box-shadow: rgba(2,132,132, 0.6) 0 0 1.5rem;
+        background: radial-gradient(transparent, rgba(163, 67, 81, 0.35));
+    }
+    
+    &:focus {
+        outline: none;
+    }
+`;
