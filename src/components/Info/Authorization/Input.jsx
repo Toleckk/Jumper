eb7e2e51ddcf7f1645fe7d72b46d0760b6848343
Toleckk <anatolich1995@gmail.com>
2019-10-24 21:51:53 +0300

    Font fixed

--- src/components/Info/Authorization/Input.jsx
+++ src/components/Info/Authorization/Input.jsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export default styled.input`
     margin-top: 15px;
     
-    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
     border-radius: 4px;
     padding: 10px 12px;
     border: 2px solid darkgrey;
@@ -15,6 +14,6 @@ export default styled.input`
     &:focus {
         outline: none;
         border-color: rgb(163, 67, 81);
-        box-shadow: rgba(163, 67, 81, 0.5) 0 0 0 0.2rem;
+        box-shadow: rgba(163, 67, 81, 0.5) 0 0 0.8rem 0.2rem;
     }
 `;
