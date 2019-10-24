5965ad828dcfdda084792357f5350635c5811899
Toleckk <anatolich1995@gmail.com>
2019-10-24 21:47:07 +0300

    Styles fixes

--- src/components/Info/Container.jsx
+++ src/components/Info/Container.jsx
@@ -9,8 +9,10 @@ export default styled.div`
     
     background: rgba(0, 0, 0, 0.2);
     color: #babac7;
+    
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+    
+    & > span {
+        margin-top: 20px;
+    }
 `;
-
-//#171848
-//background: linear-gradient(to top, #05050d, #060612, #060612, #05050d);
-//тёмный #0e0f2d
