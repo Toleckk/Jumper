9a61a2b1ec1cf0eedbb16697e57748f6af8b25e3
Toleckk <anatolich1995@gmail.com>
2019-10-25 20:19:17 +0300

    Landing picture added

---
+++ src/components/Picture/Container.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+    flex: 1;
+
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    padding-top: 11%;
+`;
---
+++ src/components/Picture/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from "./Container";
+import Triangle from "../atoms/Triangle";
+import SVG from "./SVG";
+
+const Picture = ({size = 24}) => <Container>
+    <SVG viewBox={`0 0 ${size} ${size}`}>
+        <Triangle x={1} y={1} size={size} stroke="rgba(166, 69, 86, 0.35)" fill="transparent" strokeWidth={0.8}/>
+        <Triangle x={3.4} y={2.4} size={size} stroke="rgba(166, 69, 86, 0.3)" fill="transparent" strokeWidth={0.8}/>
+    </SVG>
+</Container>;
+
+export default Picture;
