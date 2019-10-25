8bdd9e742839906878df2be89c3bda217d86cd0b
Toleckk <anatolich1995@gmail.com>
2019-10-25 22:50:20 +0300

    Shadow color changed

--- src/components/Landing/Picture/SVG.jsx
+++ src/components/Landing/Picture/SVG.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export default styled.svg`
     width: 80%;
     transform: rotate(100deg);
-    filter: blur(2px) contrast(130%) drop-shadow(0 0 1rem rgba(0, 255, 255, 0.5));
+    filter: blur(2px) contrast(130%) drop-shadow(0 0 1rem rgb(2, 132, 132));
     
     @media (min-device-aspect-ratio: 16/9) {
         width: 70%;
