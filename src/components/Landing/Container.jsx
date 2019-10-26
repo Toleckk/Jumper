a6331c7c1b7fce5106297a80bbf3e3bded13095a
Toleckk <anatolich1995@gmail.com>
2019-10-26 16:54:54 +0300

    Small styles fixes

--- src/components/Landing/Container.jsx
+++ src/components/Landing/Container.jsx
@@ -4,10 +4,11 @@ export default styled.div`
     height: 100vh;
     display: flex;
     flex-direction: row;
+    
     @media(max-width: 768px){
         flex-direction: column;
     }
-    height: -webkit-fill-available;
+    
     background-image: url('/images/landing.jpg');
     background-position-x: -130px;
     overflow: hidden;
