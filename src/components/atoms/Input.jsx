433491f7b913aa3027b71891e1d386de3b2a3f83
Toleckk <anatolich1995@gmail.com>
2019-10-27 15:09:19 +0300

    Shadow color changed, theme introduced

--- src/components/atoms/Input.jsx
+++ src/components/atoms/Input.jsx
@@ -1,20 +1,19 @@
 import styled from "styled-components";
 
-//todo: choose shadow color
 export default styled.input`
     margin-top: 15px;
     
     border-radius: 4px;
     padding: 10px 12px;
-    border: 2px solid darkgrey;
+    border: 2px solid rgba(${props => props.theme.primaryText}, 0.9);
     font-size: medium;
     background: transparent;
     transition: all 300ms;
-    color: #babac7;
+    color: rgb(${props => props.theme.primaryText});
     
     &:focus {
         outline: none;
-        border-color: rgb(163, 67, 81);
-        box-shadow: rgb(163, 67, 81) 0 0 1.4rem;
+        border-color: rgb(${props => props.theme.secondary});
+        box-shadow: rgb(${props => props.theme.secondary}) 0 0 1.4rem;
     }
 `;
