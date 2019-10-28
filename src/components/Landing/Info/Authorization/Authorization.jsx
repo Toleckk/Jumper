835fda1755454ca4150dced52e5534d1cd17e4ef
Toleckk <anatolich1995@gmail.com>
2019-10-28 12:58:10 +0300

    Formik

--- src/components/Landing/Info/Authorization/Authorization.jsx
+++ src/components/Landing/Info/Authorization/Authorization.jsx
@@ -3,11 +3,28 @@ import Input from "../../../atoms/Input";
 import Container from "./Container";
 import Button from "../../../atoms/Button";
 import local from "../../../../local";
+import {withFormik} from "formik";
+import * as Yup from "yup";
 
-const Authorization = () => <Container>
-    <Input id="login" placeholder={local.info.loginInputPlaceholder}/>
-    <Input id="password" type="password" placeholder={local.info.passwordInputPlaceholder}/>
-    <Button id="submit">{local.info.signInButton}</Button>
-</Container>;
+const form = withFormik({
+    validateOnChange: false,
+    validateOnBlur: false,
+    validateOnMount: false,
+    handleSubmit: console.log,
+    validationSchema: Yup.object().shape({
+        login: Yup.string().required(true),
+        password: Yup.string().required(true),
+    })
+});
 
-export default Authorization;
+const Authorization = ({handleChange, handleSubmit}) => {
+    const {passwordInputPlaceholder, loginInputPlaceholder} = local.info;
+
+    return <Container onSubmit={handleSubmit}>
+        <Input name="login" placeholder={loginInputPlaceholder} onChange={handleChange}/>
+        <Input name="password" type="password" placeholder={passwordInputPlaceholder} onChange={handleChange}/>
+        <Button type="submit">{local.info.signInButton}</Button>
+    </Container>
+};
+
+export default form(Authorization);
