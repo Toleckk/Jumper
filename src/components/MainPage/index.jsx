dceb6e2e8e19f34e530a17ca87a1c19b93f04d0b
Toleckk <anatolich1995@gmail.com>
2019-10-29 23:45:09 +0300

    Lazy loading added

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import Container from "./Container";
 import Picture from "./Picture";
 import {Route} from "react-router-dom";
 import Info from "./Info";
 import Main from "./Main";
+const AuthorizationPage = React.lazy(() => import('./AuthorizationPage'));
+const RegistrationPage = React.lazy(() => import('./RegistrationPage'));
 
 const Landing = () => <Container>
     <Picture/>
     <Main>
-        <Route exact path="/">
-            <Info/>
-        </Route>
-        <Route exact path="/registration">
-            <div>aaa</div>
-        </Route>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Route exact path="/">
+                <AuthorizationPage/>
+            </Route>
+            <Route exact path="/registration">
+                <RegistrationPage/>
+            </Route>
+        </Suspense>
     </Main>
 </Container>;
 
