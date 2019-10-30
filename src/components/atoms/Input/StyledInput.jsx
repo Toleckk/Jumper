2397cff520470a8e48dada4de5546a2fc313fe31
Toleckk <anatolich1995@gmail.com>
2019-10-30 19:09:12 +0300

    !important removed

--- src/components/atoms/Input/StyledInput.jsx
+++ src/components/atoms/Input/StyledInput.jsx
@@ -1,24 +1,23 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export default styled.input`
     margin-top: 15px;
     
     border-radius: 4px;
     padding: 10px 12px;
-    border: 2px solid rgba(${props => props.theme.primaryText}, 0.9);
+    border: 2px solid rgba(${props => props.error ? '193, 27, 27' : props.theme.primaryText}, 0.9);
     font-size: medium;
     background: transparent;
     transition: all 300ms;
     color: rgb(${props => props.theme.primaryText});
     
     ${props => props.error ? `
-        border-color: rgba(193, 27, 27, 0.9) !important;
-        box-shadow: inset rgba(193, 27, 27, 0.9) 0 0 1.1rem !important;
+        box-shadow: inset rgba(193, 27, 27, 0.9) 0 0 1.1rem;
     ` : ''}
     
-    &:focus {
-        outline: none;
+    ${props => !props.error && css`&:focus {
         border-color: rgb(${props => props.theme.secondary});
         box-shadow: rgb(${props => props.theme.secondary}) 0 0 1.4rem;
+      }`
     }
 `;
