80f98223356de2a6efe545d482f77b2509f37020
Toleckk <anatolich1995@gmail.com>
2019-11-03 01:08:38 +0300

    Small styles fixed

--- src/components/MainPage/RegistrationPage/Registration/Form.js
+++ src/components/MainPage/RegistrationPage/Registration/Form.js
@@ -2,5 +2,9 @@ import styled from "styled-components";
 import Flex from "../../../atoms/Flex";
 
 export default styled(Flex).attrs({as: 'form', column: true})`
-    margin-top: 15px;
+    width: 40%;
+    
+    & > * {
+        margin-top: 20px;
+    }
 `;
