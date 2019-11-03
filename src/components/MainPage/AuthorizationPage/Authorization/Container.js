42cd64064d1c191e6312099a665cee45957bee1b
Toleckk <anatolich1995@gmail.com>
2019-11-04 02:33:48 +0300

    Small styles fixes

--- src/components/MainPage/AuthorizationPage/Authorization/Container.js
+++ src/components/MainPage/AuthorizationPage/Authorization/Container.js
@@ -5,7 +5,7 @@ export default styled(Flex).attrs({as: 'form', column: true, align: 'center'})`
     margin-top: 28px;
     min-width: 100px;
     
-    & > * {
+    & > *:not(:first-child) {
         margin-top: 15px;
     }
 `;
