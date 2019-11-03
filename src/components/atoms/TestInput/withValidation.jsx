8aa09b080f347109d136542f2b833a735abca335
Toleckk <anatolich1995@gmail.com>
2019-11-04 02:28:24 +0300

    withValidation HOC added

---
+++ src/components/atoms/TestInput/withValidation.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import Type from "prop-types";
+
+const withValidation = Component => {
+    const ValidatedComponent = ({pattern, onChange, ...props}) => {
+        const [value, setValue] = useState('');
+
+        if(pattern) {
+            const checkPattern = createPatternChecker(pattern, onChange, setValue);
+
+            return <Component value={value} onChange={checkPattern} {...props}/>;
+        }
+
+        return <Component onChange={onChange} {...props}/>;
+    };
+
+    ValidatedComponent.propTypes = {
+        pattern: Type.instanceOf(RegExp),
+        onChange: Type.func
+    };
+
+    return ValidatedComponent;
+};
+
+
+const createPatternChecker = (pattern, onChange, setValue) => event => {
+    const {value} = event.target;
+
+    if(!pattern || pattern.test(value))
+        setValue(value);
+
+    return onChange && onChange(event);
+};
+
+
+
+export default withValidation;
