01dc14530be367b76543197659acfa4175a6e0c4
Toleckk <anatolich1995@gmail.com>
2019-11-04 02:35:44 +0300

    Prevent focus changing

--- src/components/atoms/Eye/index.jsx
+++ src/components/atoms/Eye/index.jsx
@@ -2,8 +2,30 @@ import React from "react";
 import StyledButton from "./StyledButton";
 import Icon from "./Icon";
 
-const Eye = props => <StyledButton type="button" {...props}>
-    <Icon/>
-</StyledButton>;
+const preventFocusChange = event => {
+    event.stopPropagation();
+    event.preventDefault();
+    return false;
+};
+
+const Eye = ({onMouseDown, onMouseUp, ...props}) => {
+    const onMousePressed = event => {
+        if (onMouseDown)
+            onMouseDown(event);
+
+        return preventFocusChange(event);
+    };
+    const onMouseReleased = event => {
+        if (onMouseUp)
+            onMouseUp(event);
+
+        return preventFocusChange(event);
+    };
+
+    return <StyledButton type="button" {...props} onMouseDown={onMousePressed} onMouseUp={onMouseReleased}>
+        <Icon/>
+    </StyledButton>;
+};
+
 
 export default Eye;
