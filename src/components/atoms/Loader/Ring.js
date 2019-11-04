e83409b9ce326a20ee3c316c7a50198280eec26e
Toleckk <anatolich1995@gmail.com>
2019-11-04 04:08:42 +0300

    Refactor

--- src/components/atoms/Loader/Ring.js
+++ src/components/atoms/Loader/Ring.js
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
 export default styled.div`
-    width: ${props => props.size}vh;
-    height: ${props => props.size}vh;
-    
-    border: ${props => props.bold}px solid rgb(${props => props.theme.primary}); 
-    border-radius: 50%;
-    
-    box-shadow: inset rgba(${props => props.theme.secondary}, 0.7) 0 0 2rem 0.5rem, 
-                rgba(${props => props.theme.secondary}, 0.7) 0 0 2rem 0.5rem;
-    opacity: 0.5;
-    
-    animation: 2s linear -1s infinite normal none running ${props => props.animation};
+    ${({animation, bold, size, theme}) => `
+        width: ${size}vh;
+        height: ${size}vh;
+        
+        border: ${bold}px solid rgb(${theme.primary}); 
+        border-radius: 50%;
+        
+        box-shadow: inset rgba(${theme.secondary}, 0.7) 0 0 2rem 0.5rem, 
+                    rgba(${theme.secondary}, 0.7) 0 0 2rem 0.5rem;
+        opacity: 0.5;
+        
+        animation: 2s linear -1s infinite normal none running ${animation};
+    `}
 `;
