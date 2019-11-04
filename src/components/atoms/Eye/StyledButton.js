94bacfd3106576d7bccf6856f4df9eba077ed63a
Toleckk <anatolich1995@gmail.com>
2019-11-04 19:04:52 +0300

    Eye improved

--- src/components/atoms/Eye/StyledButton.js
+++ src/components/atoms/Eye/StyledButton.js
@@ -1,8 +1,26 @@
 import styled from "styled-components";
 
 export default styled.button`
-    height: 25px;
-    width: 25px;
+    height: 23px;
+    width: 23px;
     
-    margin-left: -25px;
+    margin-left: -23px;
+    
+    opacity: 0.5;
+    
+    transition: all 300ms;
+    
+    &:hover {
+      opacity: 0.8;
+  }
+    
+    &:active {
+      filter: drop-shadow(0 0 0.2rem rgb(${props => props.theme.primary}));
+      opacity: 0.9;
+    }
+    
+    &:focus {
+      filter: drop-shadow(0 0 0.2rem rgb(${props => props.theme.primary}));
+      opacity: 0.7;
+    }
 `;
