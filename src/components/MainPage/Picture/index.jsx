33d24e1ecf41b470951c5cd13180f2c05f8364a6
Toleckk <anatolich1995@gmail.com>
2019-11-04 18:01:28 +0300

    Similar color removed from theme

--- src/components/MainPage/Picture/index.jsx
+++ src/components/MainPage/Picture/index.jsx
@@ -6,13 +6,13 @@ import {ThemeContext} from "styled-components";
 import Type from 'prop-types';
 
 const Picture = ({size}) => {
-    const {accents} = useContext(ThemeContext);
+    const {secondary} = useContext(ThemeContext);
     const triangleProps = {size, fill: "transparent", strokeWidth: 0.8};
 
     return <Container>
         <SVG viewBox={`0 0 ${size} ${size}`}>
-            <Triangle x={1} y={1} stroke={`rgba(${accents}, 0.35)`} {...triangleProps}/>
-            <Triangle x={3.4} y={2.4} stroke={`rgba(${accents}, 0.3)`} {...triangleProps}/>
+            <Triangle x={1} y={1} stroke={`rgba(${secondary}, 0.35)`} {...triangleProps}/>
+            <Triangle x={3.4} y={2.4} stroke={`rgba(${secondary}, 0.3)`} {...triangleProps}/>
         </SVG>
     </Container>;
 };
--- src/theme/dark.js
+++ src/theme/dark.js
@@ -1,7 +1,6 @@
 export default {
     primary: '2, 132, 132',
     secondary: '163, 67, 81',
-    accents: '166, 69, 86',
     primaryText: '186, 186, 199',
     secondaryText: '202, 202, 220',
     error: '193, 27, 27'
