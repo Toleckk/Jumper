dcdb1bba6fdb7c0a4d6a9274c2525366c24e0c66
Toleckk <anatolich1995@gmail.com>
2019-11-06 19:55:08 +0300

    Logo moved

--- src/components/MainPage/AuthorizationPage/index.jsx
+++ src/components/MainPage/AuthorizationPage/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from "../Logo";
+import Logo from "./Logo";
 import Header from "../Header";
 import Authorization from "./Authorization/Authorization";
 import Registration from "./Registration";
