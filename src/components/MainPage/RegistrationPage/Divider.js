f243256f70b828fad76a729acdb6399d0e1f967f
Toleckk <anatolich1995@gmail.com>
2019-11-06 19:56:46 +0300

    Divider for registration added

---
+++ src/components/MainPage/RegistrationPage/Divider.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.hr`
+    width: 100%;
+    height: 1px;
+    background: rgb(${props => props.theme.primary});
+    margin: 1.1em 0;
+    box-shadow: rgb(${props => props.theme.primary}) 0 0 1rem 0.05rem;
+`;
