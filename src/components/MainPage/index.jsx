b1a32299c4e22ebbd2948385b626f4b56ab46250
Toleckk <anatolich1995@gmail.com>
2019-11-06 18:31:00 +0300

    Document title added

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,9 +1,11 @@
 import React, {Suspense} from 'react';
+import Title from "react-document-title";
 import Container from "./Container";
 import Picture from "./Picture";
 import {Route} from "react-router-dom";
 import Main from "./Main";
 import Loader from "../atoms/Loader";
+
 const AuthorizationPage = React.lazy(() => import('./AuthorizationPage'));
 const RegistrationPage = React.lazy(() => import('./RegistrationPage'));
 
@@ -15,7 +17,9 @@ const MainPage = () => <Container>
                 <AuthorizationPage/>
             </Route>
             <Route exact path="/registration">
-                <RegistrationPage/>
+                <Title title="Registration">
+                    <RegistrationPage/>
+                </Title>
             </Route>
         </Suspense>
     </Main>
