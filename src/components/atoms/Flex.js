2eeedfbeaec86d152fe5707b7bc47f8a405399b0
Toleckk <anatolich1995@gmail.com>
2019-11-06 22:20:18 +0300

    Justify prop added to Flex component

--- src/components/atoms/Flex.js
+++ src/components/atoms/Flex.js
@@ -5,11 +5,13 @@ const Flex = styled.div`
     display: flex;
     flex-direction: ${props => props.column ? 'column' : 'row'};
     align-items: ${props => props.align || 'initial'};
+    justify-content: ${props => props.justify || 'initial'};
 `;
 
 Flex.propTypes = {
     column: Type.bool,
-    align: Type.oneOf(['center', 'flex-start', 'flex-end'])
+    align: Type.oneOf(['center', 'flex-start', 'flex-end']),
+    justify: Type.oneOf(['flex-start', 'flex-end', 'space-between', 'space-around'])
 };
 
 export default Flex;
