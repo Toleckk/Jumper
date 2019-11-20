09cb16490bb0f6d6a8448854092c6e9d4510a523
Toleckk <anatolich1995@gmail.com>
2019-11-20 03:00:14 +0300

    Routing for new components added

--- src/components/Landing/Restore/index.jsx
+++ src/components/Landing/Restore/index.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import {useLocalizationContext} from "contexts/Localization";
-import Header from "../atoms/Header";
+import {Redirect, Route} from "react-router-dom";
 import Logo from "../Authorization/atoms/Logo";
-import StyledSpan from "./atoms/StyledSpan";
-import Divider from "../atoms/Divider";
-import RestoreForm from "./RestoreForm";
-import LargeSpan from "../atoms/LargeSpan";
+import Login from "./Login";
+import Message from "./Message";
+import Password from "./Password";
 
-const Restore = () => {
-    const {restore} = useLocalizationContext();
-
-    return <>
-        <Logo/>
-        <Header>{restore.header}</Header>
-        <LargeSpan>{restore.info}</LargeSpan>
-        <Divider/>
-        <StyledSpan>{restore.login}</StyledSpan>
-        <RestoreForm/>
-        <Divider/>
-    </>;
-};
+const Restore = () => <>
+    <Logo src="" alt=""/>
+    <Route path="/restore/login">
+        <Login/>
+    </Route>
+    <Route path="/restore/message">
+        <Message/>
+    </Route>
+    <Route path="/restore/password">
+        <Password/>
+    </Route>
+    <Route exact path="/restore">
+        <Redirect to="/restore/login"/>
+    </Route>
+</>;
 
 export default React.memo(Restore);
