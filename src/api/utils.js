2d2c3b3480f3dca6f7a327f22bf6804a1d34fff3
Toleckk <anatolich1995@gmail.com>
2019-12-08 13:31:19 +0300

    Refactoring

--- src/api/utils.js
+++ src/api/utils.js
@@ -8,17 +8,19 @@ export const post = (url, params) => request(url, {
     }
 });
 
-const request = (url, params = {headers: {}}) => {
-    return fetch(url, {
-        ...params,
-        credentials: 'same-origin',
-        header: {
-            ...params.headers,
-            Accept: 'application/json',
-        }
-    }).then(response => {
-        if(!response.ok)
-            throw new Error(response.status.toString());
-        return response;
-    });
+const request = (url, params = {headers: {}}) => fetch(url, makeConfig(params)).then(handleErrors);
+
+const makeConfig = params => ({
+    ...params,
+    credentials: 'same-origin',
+    header: {
+        ...params.headers,
+        Accept: 'application/json',
+    }
+});
+
+const handleErrors = response => {
+    if (!response.ok)
+        throw new Error(response.status.toString());
+    return response;
 };
