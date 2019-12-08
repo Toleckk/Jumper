77988041f24d4e6e68f44327940e9b2e9f99e28e
Toleckk <anatolich1995@gmail.com>
2019-12-08 13:15:42 +0300

    Restore api injected

--- src/components/Landing/Restore/Login/RestoreForm.jsx
+++ src/components/Landing/Restore/Login/RestoreForm.jsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useHistory} from 'react-router-dom';
 import {useTranslation} from "contexts/Localization";
 import StyledForm from "../atoms/StyledForm";
 import StyledButton from "../atoms/StyledButton";
 import StyledInput from "../atoms/StyledInput";
 import StyledSpan from "../atoms/StyledSpan";
+import {restore} from "api";
 
 const loginPattern = /^[-_0-9A-Za-z.@]*$/;
 
 const validate = ({login}) => ({login: !login || !login.length});
 
-const RestoreForm = () => {
+const RestoreForm = ({setLoaded}) => {
     const {t} = useTranslation();
+    const history = useHistory();
+    // eslint-disable-next-line no-unused-vars
+    const [errors, setErrors] = useState(false);
 
-    return <StyledForm onSubmit={console.log} validate={validate} resetFieldErrorOnChange>{({onChange, errors}) => (
+    const submit = data => {
+        setLoaded(true);
+        restore(data)
+            .then(() => history.push('/restore/message'))
+            .catch(setErrors)
+            .finally(() => setLoaded(false))
+    };
+
+    return <StyledForm onSubmit={submit} validate={validate} resetFieldErrorOnChange>{({onChange, errors, updateState}) => (
         <>
             <StyledSpan>{t('Enter your login')}</StyledSpan>
             <StyledInput name="login"
