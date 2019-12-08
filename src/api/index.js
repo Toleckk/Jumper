f2f0145b9e5c9ed7f04151d33ad72cf8f51d2650
Toleckk <anatolich1995@gmail.com>
2019-12-08 13:14:07 +0300

    Restore api added

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import login from './login';
 import registration from "./registration";
 import confirmRegistration from "./confirmRegistration";
+import restore from "./restore";
 
-export {login, registration, confirmRegistration};
+export {login, registration, confirmRegistration, restore};
