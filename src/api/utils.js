7ca2360c8d2beebd2badfd57e13a45d1c6126b6b
Toleckk <anatolich1995@gmail.com>
2019-12-08 13:29:48 +0300

    Errors handling added

--- src/api/utils.js
+++ src/api/utils.js
@@ -8,11 +8,17 @@ export const post = (url, params) => request(url, {
     }
 });
 
-const request = (url, params = {headers: {}}) => fetch(url, {
-    ...params,
-    credentials: 'same-origin',
-    header: {
-        ...params.headers,
-        Accept: 'application/json',
-    }
-});
+const request = (url, params = {headers: {}}) => {
+    return fetch(url, {
+        ...params,
+        credentials: 'same-origin',
+        header: {
+            ...params.headers,
+            Accept: 'application/json',
+        }
+    }).then(response => {
+        if(!response.ok)
+            throw new Error(response.status.toString());
+        return response;
+    });
+};
