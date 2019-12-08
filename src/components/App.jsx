261c0ba16392288ab5c6321400abf1c072d094ee
Toleckk <anatolich1995@gmail.com>
2019-12-08 23:18:23 +0300

    UserPage navigation added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,37 @@
 import React, {useEffect} from 'react';
 import {useQuery} from "@apollo/react-hooks";
+import {Route, Switch, useHistory} from "react-router-dom";
 import {client} from "apollo";
 import {ME} from "apollo/requests/user";
 import {ME as STORED_ME} from "apollo/actions/user";
 import {BigLoader} from "./Common/molecules";
 import Landing from "./Landing";
+import UserPage from "./UserPage";
 
 const App = () => {
     const {loading, data} = useQuery(STORED_ME);
+    const history = useHistory();
 
     useEffect(() => {
         client.query({query: ME});
     }, []);
 
+    useEffect(() => {
+        if (data && data.me)
+            history.replace(`/user/${data.me.nickname}`);
+    }, [data]);
+
     if (loading)
         return <BigLoader/>;
-    if (!data || !data.me)
-        return <Landing/>;
-    return <></>;
+
+    return <Switch>
+        <Route exact path="/user/:nickname">
+            <UserPage/>
+        </Route>
+        <Route path="/">
+            <Landing/>
+        </Route>
+    </Switch>;
 };
 
 export default React.memo(App);
