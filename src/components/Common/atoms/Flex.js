9248f486ad4425732be09ab3a1659452ab0c56b7
Toleckk <anatolich1995@gmail.com>
2019-12-09 11:38:55 +0300

    Justify center added to flex

--- src/components/Common/atoms/Flex.js
+++ src/components/Common/atoms/Flex.js
@@ -11,7 +11,7 @@ const Flex = styled.div`
 Flex.propTypes = {
     direction: Type.oneOf(['row', 'column', 'row-reverse', 'column-reverse']),
     align: Type.oneOf(['center', 'flex-start', 'flex-end']),
-    justify: Type.oneOf(['flex-start', 'flex-end', 'space-between', 'space-around']),
+    justify: Type.oneOf(['flex-start', 'flex-end', 'space-between', 'space-around', 'center']),
 };
 
 export default Flex;
