c95f907e2b878c31f35ab9bf8bee31b1797a230b
Toleckk <anatolich1995@gmail.com>
2019-12-26 22:31:38 +0300

    Apollo client provided

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "styled-components";
 import {LocalizationProvider} from "contexts/Localization";
+import {ApolloProvider} from "@apollo/react-hooks";
+import {client} from "apollo";
 import dark from "theme/dark";
 import Landing from "./Landing";
 
 
 const App = () =>
-    <BrowserRouter>
+    <ApolloProvider client={client}>
         <LocalizationProvider lang="ru">
             <ThemeProvider theme={dark}>
-                <Landing/>
+                <BrowserRouter>
+                    <Landing/>
+                </BrowserRouter>
             </ThemeProvider>
         </LocalizationProvider>
-    </BrowserRouter>;
+    </ApolloProvider>;
 
 export default React.memo(App);
