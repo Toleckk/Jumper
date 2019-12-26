ae600a417991f4992743de10f3c012576f742a63
Toleckk <anatolich1995@gmail.com>
2019-12-26 22:43:04 +0300

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
-import {ThemeProvider} from "styled-components";
-import {LocalizationProvider} from "Common/contexts/Localization";
-import {ApolloProvider} from "@apollo/react-hooks";
-import {client} from "Common/apollo";
-import dark from "Common/theme/dark";
 import {Landing} from "./Landing";
 
 
-const App = () =>
-    <ApolloProvider client={client}>
-        <LocalizationProvider lang="ru">
-            <ThemeProvider theme={dark}>
-                <BrowserRouter>
-                    <Landing/>
-                </BrowserRouter>
-            </ThemeProvider>
-        </LocalizationProvider>
-    </ApolloProvider>;
+const App = () => <Landing/>;
 
 export default React.memo(App);
