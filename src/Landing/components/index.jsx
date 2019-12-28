01ffe54c43b6f55df8a62d0696d288c267c3a2ca
Toleckk <anatolich1995@gmail.com>
2019-12-29 00:00:36 +0300

    Loading context provided

--- src/Landing/components/index.jsx
+++ src/Landing/components/index.jsx
@@ -5,30 +5,33 @@ import {Loader} from "Common/molecules";
 import Container from "./atoms/Container";
 import Picture from "./Picture";
 import Main from "./atoms/Main";
+import {LoadingContextProvider} from "../contexts/LoadingContext";
 
 const AuthorizationPage = React.lazy(() => import('./Authorization'));
 const RegistrationPage = React.lazy(() => import('./Registration'));
 const RestorePage = React.lazy(() => import('./Restore'));
 
-const Landing = () => <Container>
-    <Picture/>
-    <Main>
-        <Suspense fallback={<Loader/>}>
-            <Route exact path="/">
-                <AuthorizationPage/>
-            </Route>
-            <Route path="/registration">
-                <Title title="Registration">
-                    <RegistrationPage/>
-                </Title>
-            </Route>
-            <Route path="/restore">
-                <Title title="Restore">
-                    <RestorePage/>
-                </Title>
-            </Route>
-        </Suspense>
-    </Main>
-</Container>;
+const Landing = () => <LoadingContextProvider>
+    <Container>
+        <Picture/>
+        <Main>
+            <Suspense fallback={<Loader/>}>
+                <Route exact path="/">
+                    <AuthorizationPage/>
+                </Route>
+                <Route path="/registration">
+                    <Title title="Registration">
+                        <RegistrationPage/>
+                    </Title>
+                </Route>
+                <Route path="/restore">
+                    <Title title="Restore">
+                        <RestorePage/>
+                    </Title>
+                </Route>
+            </Suspense>
+        </Main>
+    </Container>
+</LoadingContextProvider>;
 
 export default React.memo(Landing);
