0039db5a9cd145c2803261a66c090f1624866a0e
Toleckk <anatolich1995@gmail.com>
2019-12-29 00:01:13 +0300

    Loader for template added

---
+++ src/Landing/components/templates/MainPageTemplate/Loader/InternalContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export default styled.div`
+    display: flex;
+    width: 50vw;
+    height: 100%;
+    position: fixed;
+    flex-direction: column;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.5);
+`;
--- src/Landing/components/templates/MainPageTemplate/index.jsx
+++ src/Landing/components/templates/MainPageTemplate/index.jsx
@@ -4,8 +4,10 @@ import Header from "../../atoms/Header";
 import LargeSpan from "../../atoms/LargeSpan";
 import Divider from "../../atoms/Divider";
 import Logo from "./Logo";
+import Loader from "./Loader";
 
-const MainPageTemplate = ({logo, header, description, content: Main, footer: Footer, withLastDivider}) => <>
+const MainPageTemplate = ({logo, loading, header, description, content: Main, footer: Footer, withLastDivider}) => <>
+    {loading && <Loader/>}
     <Logo large={logo === 'large'} src="" alt=""/>
     <Header>{header}</Header>
     <LargeSpan>{description}</LargeSpan>
@@ -19,6 +21,7 @@ MainPageTemplate.defaultProps = {
     footer: null,
     logo: 'small',
     withLastDivider: false,
+    loading: false,
 };
 
 MainPageTemplate.propTypes = {
@@ -28,6 +31,7 @@ MainPageTemplate.propTypes = {
     content: Type.elementType.isRequired,
     footer: Type.elementType,
     withLastDivider: Type.bool,
+    loading: Type.bool,
 };
 
 export default React.memo(MainPageTemplate);
