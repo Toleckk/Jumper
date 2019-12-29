294956013e665c4240a1bc4c7d83e6fc9aa94bc6
Toleckk <anatolich1995@gmail.com>
2019-12-30 02:57:15 +0300

    Unused loading removed

--- src/Landing/components/Registration/Email/index.jsx
+++ src/Landing/components/Registration/Email/index.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 import {useLocalizationContext} from "Common/contexts/Localization";
 import EmailForm from "./EmailForm";
 import {MainPageTemplate} from "../../templates"
-import {useLoadingContext} from "../../../contexts/LoadingContext";
 
 const Email = () => {
     const {t} = useLocalizationContext();
-    const {loading} = useLoadingContext();
 
-    return <MainPageTemplate loading={loading}
-                             header={t('create')}
+    return <MainPageTemplate header={t('create')}
                              description={t('You are on registration page')}
                              content={EmailForm}
     />;
