4cacddb6792e9c5190d23f1f140dabd93e6ba0bf
Toleckk <anatolich1995@gmail.com>
2019-12-30 02:52:59 +0300

    Loader removed

--- src/Landing/components/templates/MainPageTemplate/index.jsx
+++ src/Landing/components/templates/MainPageTemplate/index.jsx
@@ -4,10 +4,8 @@ import Header from "../../atoms/Header";
 import LargeSpan from "../../atoms/LargeSpan";
 import Divider from "../../atoms/Divider";
 import Logo from "./Logo";
-import Loader from "./Loader";
 
-const MainPageTemplate = ({logo, loading, header, description, content: Main, footer: Footer, withLastDivider}) => <>
-    {loading && <Loader/>}
+const MainPageTemplate = ({logo, header, description, content: Main, footer: Footer, withLastDivider}) => <>
     <Logo large={logo === 'large'} src="" alt=""/>
     <Header>{header}</Header>
     <LargeSpan>{description}</LargeSpan>
@@ -31,7 +29,6 @@ MainPageTemplate.propTypes = {
     content: Type.elementType.isRequired,
     footer: Type.elementType,
     withLastDivider: Type.bool,
-    loading: Type.bool,
 };
 
 export default React.memo(MainPageTemplate);
