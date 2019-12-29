e347efda3d8dda673a89cbd1102f7d9c67aad837
Toleckk <anatolich1995@gmail.com>
2019-12-30 02:51:55 +0300

    Interpolation added

--- src/Common/contexts/Localization/translate.js
+++ src/Common/contexts/Localization/translate.js
@@ -1,1 +1,17 @@
-export default lang => key => lang[key] || '';
+export default lang => (key, vars) => {
+    const translation = lang[key];
+
+    if(!translation)
+        return key || '';
+
+    return vars ? injectVars(translation, vars) : translation;
+}
+
+const injectVars = (translation, vars) => {
+    const entries = Object.entries(vars);
+    const replace = (trans, [key, value]) => trans.replace(regExpOf(key), value);
+
+    return entries.reduce(replace, translation);
+};
+
+const regExpOf = key => new RegExp(`{{${key}}}`, 'g');
