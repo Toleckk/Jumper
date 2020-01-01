726f96073bb0fcdd9c2b9798d8e4e7ecf612125e
Toleckk <anatolich1995@gmail.com>
2020-01-01 03:26:40 +0300

    Animation added

--- src/Landing/components/atoms/Divider.js
+++ src/Landing/components/atoms/Divider.js
@@ -1,4 +1,14 @@
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const appear = keyframes`
+  from {
+    transform: scaleX(0);
+  }
+  
+  to {
+    transform: scaleX(1);    
+  }
+`;
 
 export default styled.hr`
     width: 100%;
@@ -7,4 +17,6 @@ export default styled.hr`
     background: rgb(${props => props.theme.primary});
     margin: 3vh 0;
     box-shadow: rgb(${props => props.theme.primary}) 0 0 1rem 0.05rem;
+    
+    animation: ${appear} 880ms ease normal;
 `;
