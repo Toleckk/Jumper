b2196b5f31e03ee815f9a4bdbe15486298254154
Toleckk <anatolich1995@gmail.com>
2020-01-04 15:28:04 +0300

    Registration password form rewrote for RFF

--- src/Landing/components/Registration/Password/PasswordForm.jsx
+++ src/Landing/components/Registration/Password/PasswordForm.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {useParams} from 'react-router-dom';
 import {useMutation} from "@apollo/react-hooks";
 import {useLocalizationContext} from "Common/contexts/Localization";
-import {Form} from 'Common/molecules';
+import {Field, Form} from "react-final-form";
 import {CONFIRM} from "Landing/mutations/registration";
+import {Loader} from "Common/molecules";
 import StyledForm from "../atoms/StyledForm";
 import StyledButton from "../atoms/StyledButton";
 import Divider from "../../atoms/Divider";
@@ -11,60 +12,63 @@ import Row from "../atoms/Row";
 import StyledInput from "../atoms/StyledInput";
 
 const isValidPassword = text => (
-    /[a-z]+/.test(text) && /[A-Z]+/.test(text) && /\d/.test(text) && text.length >= 8 && text.length <= 50
+    text
+    && /[a-z]+/.test(text)
+    && /[A-Z]+/.test(text)
+    && /\d/.test(text)
+    && text.length >= 8
+    && text.length <= 50
 );
-
-const validate = ({password, confirm}) => ({
-    password: !password || !password.length || !isValidPassword(password),
-    confirm: !confirm || !confirm.length || confirm !== password,
-});
-
-const validateOnChange = ({password, confirm}) => ({
-    password: password && password.length && !isValidPassword(password),
-    confirm: confirm && confirm.length && password !== confirm,
-});
 
 const PasswordForm = () => {
     const {t} = useLocalizationContext();
     const {token} = useParams();
 
-    const [confirmRegistration] = useMutation(CONFIRM, {variables: {token}});
+    const [confirmRegistration, {loading}] = useMutation(CONFIRM, {variables: {token}});
+    // TODO: redirect
     const submit = ({password}) => confirmRegistration({variables: {password}});
 
-    return <Form as={StyledForm}
-                 onSubmit={submit}
-                 validate={validate}
-                 validateOnChange={validateOnChange}
-                 resetFieldErrorOnChange
-    >{({updateState, errors, onChange}) => (
-        <>
+    const validate = ({password, confirm}) => {
+        if(isValidPassword(password) && isValidPassword(confirm) && password === confirm)
+            return false;
+
+        return {
+            password: !isValidPassword(password),
+            confirm: confirm !== password,
+        };
+    };
+
+    return <Form onSubmit={submit} validate={validate}>{({handleSubmit}) => <>
+        {loading && <Loader background={"dark"}/>}
+        <StyledForm onSubmit={handleSubmit}>
             <Row>
                 <label htmlFor="password">{t('Password format')}</label>
-                <StyledInput id="password"
-                             name="password"
-                             password
-                             legend={t('password')}
-                             onBlur={updateState}
-                             onChange={onChange}
-                             error={errors.password}
-                />
+                <Field name="password">{({input, meta: {error}}) => (
+                    <StyledInput id="password"
+                                 password
+                                 legend={t('password')}
+                                 error={input.value && error}
+                                 {...input}
+                    />
+                )}</Field>
             </Row>
             <Divider/>
             <Row>
                 <label htmlFor="confirm">{t('Enter password again')}</label>
-                <StyledInput id="confirm"
-                             name="confirm"
-                             password
-                             legend={t('Confirm password')}
-                             onChange={onChange}
-                             onBlur={updateState}
-                             error={errors.confirm}
-                />
+                <Field name="confirm">{({input, meta: {error}}) => (
+                    <StyledInput id="confirm"
+                                 password
+                                 legend={t('Confirm password')}
+                                 error={input.value && error}
+                                 {...input}
+                    />
+                )}</Field>
             </Row>
             <Divider/>
             <StyledButton>{t('next')}</StyledButton>
-        </>
-    )}</Form>;
+        </StyledForm>
+    </>
+    }</Form>
 };
 
 export default React.memo(PasswordForm);
