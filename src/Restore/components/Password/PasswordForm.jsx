dc2bfb1dbd4cd44531d7bb0655f83d039662e2a0
Toleckk <anatolich1995@gmail.com>
2020-01-05 01:13:49 +0300

    Restore password form rewrote to RFF

--- src/Restore/components/Password/PasswordForm.jsx
+++ src/Restore/components/Password/PasswordForm.jsx
@@ -1,56 +1,37 @@
 import React from 'react';
 import {useMutation} from "@apollo/react-hooks";
 import {useParams} from "react-router-dom";
+import {Field, Form} from "react-final-form";
 import {useTranslation} from "Common/contexts/Localization";
+import {Loader} from "Common/molecules";
+import useValidation from "Common/hooks/useValidation";
 import StyledButton from "../atoms/StyledButton";
 import StyledForm from "../atoms/StyledForm";
 import StyledInput from "../atoms/StyledInput";
 import {CONFIRM} from "../../mutations/restore";
 
-// TODO: remove
-const isValidPassword = text => (
-    /[a-z]+/.test(text) && /[A-Z]+/.test(text) && /\d/.test(text) && text.length >= 8 && text.length <= 50
-);
-
-const validate = ({password, confirm}) => ({
-    password: !password || !password.length || !isValidPassword(password),
-    confirm: !confirm || !confirm.length || confirm !== password,
-});
-
-const validateOnChange = ({password, confirm}) => ({
-    password: password && password.length && !isValidPassword(password),
-    confirm: confirm && confirm.length && password !== confirm,
-});
-
 const PasswordForm = () => {
     const {t} = useTranslation();
     const {token} = useParams();
-    const [confirmRegistration] = useMutation(CONFIRM, {variables: {token}});
-    const submit = data => confirmRegistration({variables: {...data}});
+    const {passwordCreation} = useValidation();
 
-    return <StyledForm onSubmit={submit}
-                       validate={validate}
-                       validateOnChange={validateOnChange}
-                       resetFieldErrorOnChange
-    >{({updateState, errors, onChange}) => (
-        <>
-            <StyledInput name="password"
-                         password
-                         legend={t('password')}
-                         onBlur={updateState}
-                         onChange={onChange}
-                         error={errors.password}
-            />
-            <StyledInput name="confirm"
-                         password
-                         legend={t('Confirm password')}
-                         onChange={onChange}
-                         onBlur={updateState}
-                         error={errors.confirm}
-            />
+    const [confirmRestore, {loading}] = useMutation(CONFIRM, {variables: {token}});
+    // TODO: redirect
+    const submit = ({password}) => confirmRestore({variables: {password}});
+
+    return <Form onSubmit={submit} validate={passwordCreation}>{({handleSubmit}) => <>
+        {loading && <Loader background={"dark"}/>}
+        <StyledForm onSubmit={handleSubmit}>
+            <Field name="password">{({input, meta: {error}}) =>
+                <StyledInput password legend={t('password')} {...input} error={input.value && error}/>
+            }</Field>
+            <Field name="confirm">{({input, meta: {error}}) =>
+                <StyledInput password legend={t('Confirm password')} {...input} error={input.value && error}/>
+            }</Field>
+
             <StyledButton>{t('next')}</StyledButton>
-        </>
-    )}</StyledForm>;
+        </StyledForm>
+    </>}</Form>
 };
 
 export default React.memo(PasswordForm);
