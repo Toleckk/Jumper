5d62261f4c38b2f7d6ed25eaf320ff6acc07de1f
Toleckk <anatolich1995@gmail.com>
2020-01-04 23:09:57 +0300

    Restore login form rewrote to RFF

--- src/Restore/components/Login/RestoreForm.jsx
+++ src/Restore/components/Login/RestoreForm.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 import {useMutation} from "@apollo/react-hooks";
+import {Form, Field} from "react-final-form";
+import {useHistory} from "react-router-dom";
 import {useTranslation} from "Common/contexts/Localization";
 import useValidation from "Common/hooks/useValidation";
+import {Loader} from "Common/molecules";
 import StyledForm from "../atoms/StyledForm";
 import StyledButton from "../atoms/StyledButton";
 import StyledInput from "../atoms/StyledInput";
 import {CREATE} from "../../mutations/restore";
 
-// TODO: remove
-const validate = ({login}) =>  ({login: !login || !login.length});
-
 const RestoreForm = () => {
     const {t} = useTranslation();
-    const [createRestore] = useMutation(CREATE);
-    const submit = data => createRestore({variables: {email: data.login}});
-    const {login} = useValidation();
+    const [createRestore, {loading}] = useMutation(CREATE);
+    const {login, email, nickname} = useValidation();
+    const history = useHistory();
 
-    return <StyledForm onSubmit={submit} validate={validate} resetFieldErrorOnChange>{({onChange, updateState, errors}) => (
-        <>
-            <StyledInput name="login"
-                         legend={t('login')}
-                         onChange={onChange}
-                         onBlur={updateState}
-                         error={errors.login}
-                         regex={login}
-            />
+    const submit = async ({login}) => {
+        if (!login)
+            return {login: true};
+
+        if (!email.test(login) && !nickname.test(login))
+            return {login: 'Invalid login'};
+
+        try {
+            // TODO: state with masked email
+            await createRestore({variables: {login}});
+            history.push('/restore/message');
+        } catch (e) {
+            // TODO: error context
+            return e.graphQLErrors[0].extensions;
+        }
+    };
+
+    return <Form onSubmit={submit}>{({handleSubmit}) => <>
+        {loading && <Loader background={"dark"}/>}
+        <StyledForm onSubmit={handleSubmit}>
+
+            <Field name="login">{({input, meta: {submitFailed, dirtySinceLastSubmit, submitError}}) =>
+                <StyledInput legend={t('login')}
+                             regex={login}
+                             {...input}
+                             error={submitFailed && !dirtySinceLastSubmit && t(submitError)}
+                />
+            }</Field>
+
             <StyledButton type="submit">{t('next')}</StyledButton>
-        </>
-    )}</StyledForm>;
+        </StyledForm>
+    </>}</Form>;
 };
 
 export default React.memo(RestoreForm);
--- src/Restore/components/atoms/StyledForm.js
+++ src/Restore/components/atoms/StyledForm.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 import {Flex} from "Common/atoms";
-import {Form} from "Common/molecules";
 
-export default styled(Flex).attrs({as: Form, direction: 'column', align: 'center'})`
+export default styled(Flex).attrs({as: 'form', direction: 'column', align: 'center'})`
     width: 35%;
 `;
