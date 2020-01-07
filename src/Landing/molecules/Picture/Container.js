de384015c1644d0a5187270d64ec4a6ff21c4c21
Toleckk <anatolich1995@gmail.com>
2020-01-08 01:17:08 +0300

    Adaptive fixes

--- src/Landing/molecules/Picture/Container.js
+++ src/Landing/molecules/Picture/Container.js
@@ -4,9 +4,10 @@ export default styled.aside`
     flex: 1;
 
     margin-top: calc(26vh - 40vw * ${Math.sin(Math.PI / 18)} + 1.5rem);
-    min-height: calc(40vw * (${Math.sin(Math.PI / 18) + Math.cos(Math.PI / 18)})); 
     
     @media (min-width: 320px) and (max-width: 480px) {
       display: none;
     }
 `;
+
+//    min-height: calc(40vw * (${Math.sin(Math.PI / 18) + Math.cos(Math.PI / 18)}));
