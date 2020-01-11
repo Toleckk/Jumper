48267363be36b3db7ffc20e448bbedf6b0961115
Toleckk <anatolich1995@gmail.com>
2020-01-11 19:41:50 +0300

    UserStack added

---
+++ src/User/molecules/UserStack.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Type from "prop-types";
+import {Flex} from "Common/atoms";
+import Avatar from "../atoms/Avatar";
+
+const props = {alt: '', border: true, size: 'small'};
+
+const UserStack = ({users}) => {
+    const [first, ...rest] = users;
+
+    return <Flex>
+        <Avatar key={0} src={first.avatar} {...props}/>
+        {rest.map(({avatar}, i) => (
+            <Avatar key={i + 1} src={avatar} stacked {...props}/>
+        ))}
+    </Flex>;
+};
+
+UserStack.propTypes = {
+    users: Type.arrayOf(Type.shape({
+        avatar: Type.string,
+        nickname: Type.string.isRequired,
+    })).isRequired
+};
+
+export default React.memo(UserStack);
