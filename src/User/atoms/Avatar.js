771f9ed7a03fbe525a3c08ed8a3f3cf3a95b712c
Toleckk <anatolich1995@gmail.com>
2020-01-11 03:03:56 +0300

    Border customization added

--- src/User/atoms/Avatar.js
+++ src/User/atoms/Avatar.js
@@ -2,24 +2,36 @@ import styled from "styled-components";
 import Type from "prop-types";
 
 const sizes = {
-    big: 155,
-    small: 40,
+    big: {
+        size: '155px',
+        // TODO: offset
+    },
+    small: {
+        size: '35px',
+        offset: '-20px',
+    },
 };
 
 const Avatar = styled.img`
+    user-select: none;
     border-radius: 50%;
-    width: ${props => sizes[props.size]}px;
-    height: ${props => sizes[props.size]}px;
-    ${props => props.stacked && `margin-left: -${sizes[props.size] / 2}px;`}
+    width: ${props => sizes[props.size].size};
+    height: ${props => sizes[props.size].size};
+    ${props => props.stacked && `margin-left: ${sizes[props.size].offset}`}
+    ${props => props.border && `border: 2px solid rgb(${props.theme.primary});
+                                box-shadow: rgb(${props.theme.primary}) 0 0 10px;
+    `}
 `;
 
 Avatar.defaultProps = {
     size: 'big',
     stacked: false,
+    border: false,
 };
 
 Avatar.propTypes = {
     size: Type.oneOf(['big', 'small']),
+    border: Type.bool,
     stacked: Type.bool,
 };
 
