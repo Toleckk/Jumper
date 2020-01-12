a420dbd9514c6eef8d0dd25763ea31babacd449b
Toleckk <anatolich1995@gmail.com>
2020-01-12 22:49:27 +0300

    Style fixes

--- src/User/atoms/Avatar.js
+++ src/User/atoms/Avatar.js
@@ -3,24 +3,30 @@ import Type from "prop-types";
 
 const sizes = {
     big: {
-        size: '155px',
+        size: '150px',
         // TODO: offset
     },
     small: {
         size: '35px',
-        offset: '-20px',
+        offset: '-18px',
     },
 };
 
 const Avatar = styled.img`
     user-select: none;
+    cursor: pointer;
     border-radius: 50%;
     width: ${props => sizes[props.size].size};
     height: ${props => sizes[props.size].size};
     ${props => props.stacked && `margin-left: ${sizes[props.size].offset}`}
-    ${props => props.border && `border: 2px solid rgb(${props.theme.primary});
-                                box-shadow: rgb(${props.theme.primary}) 0 0 10px;
+    ${props => props.border && `border: ${props.size === 'big' ? 4 : 2}px solid rgb(${props.theme.primary});
+                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 25 : 10}px;
     `}
+    background: rgb(${props => props.theme.primaryDark});
+    
+    &:hover {
+      filter: brightness(0.8);
+    }
 `;
 
 Avatar.defaultProps = {
