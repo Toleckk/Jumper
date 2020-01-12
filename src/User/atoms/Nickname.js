c1bd074dac009263cc6e741bbdf055ff5cf2c16f
Toleckk <anatolich1995@gmail.com>
2020-01-12 22:49:50 +0300

    Text color added

--- src/User/atoms/Nickname.js
+++ src/User/atoms/Nickname.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export default styled.span`
     font-size: x-large;
     font-weight: 700;
+    color: rgb(${props => props.theme.secondaryText});
+    filter: drop-shadow(0px 0px 0.4rem rgba(${props => props.theme.primaryText}, 0.8));
 `;
