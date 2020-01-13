90d99200261f11871e228912ecb8f4c1e05fd29d
Toleckk <anatolich1995@gmail.com>
2020-01-13 04:05:16 +0300

    Styles added

--- src/User/atoms/Avatar.js
+++ src/User/atoms/Avatar.js
@@ -4,11 +4,11 @@ import Type from "prop-types";
 const sizes = {
     big: {
         size: '150px',
-        // TODO: offset
+        mobile: '100px'
     },
     small: {
-        size: '35px',
-        offset: '-18px',
+        size: '30px',
+        offset: '-12px',
     },
 };
 
@@ -18,27 +18,38 @@ const Avatar = styled.img`
     border-radius: 50%;
     width: ${props => sizes[props.size].size};
     height: ${props => sizes[props.size].size};
+    
+    @media(max-width: 768px) {
+      width: ${props => sizes[props.size].mobile || sizes[props.size].size};
+      height: ${props => sizes[props.size].mobile || sizes[props.size].size};
+      ${props => props.border && `border: ${props.size === 'big' ? 2 : 1}px solid rgb(${props.theme.primary});
+                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 20 : 1}px;
+      `}
+    }
+    
     ${props => props.stacked && `margin-left: ${sizes[props.size].offset}`}
-    ${props => props.border && `border: ${props.size === 'big' ? 4 : 2}px solid rgb(${props.theme.primary});
-                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 25 : 10}px;
+    ${props => props.border && `border: ${props.size === 'big' ? 4 : 1}px solid rgb(${props.theme.primary});
+                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 25 : 1}px;
     `}
     background: rgb(${props => props.theme.primaryDark});
     
-    &:hover {
+    ${props => props.hover && `&:hover {
       filter: brightness(0.8);
-    }
+    }`}
 `;
 
 Avatar.defaultProps = {
     size: 'big',
     stacked: false,
     border: false,
+    hover: false,
 };
 
 Avatar.propTypes = {
     size: Type.oneOf(['big', 'small']),
     border: Type.bool,
     stacked: Type.bool,
+    hover: Type.bool,
 };
 
 export default Avatar;
