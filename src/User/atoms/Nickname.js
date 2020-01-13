b6e4b0c948884e6cece85904fcb9edc9e62a71ef
Toleckk <anatolich1995@gmail.com>
2020-01-13 04:04:58 +0300

    Media query added

--- src/User/atoms/Nickname.js
+++ src/User/atoms/Nickname.js
@@ -5,4 +5,8 @@ export default styled.span`
     font-weight: 700;
     color: rgb(${props => props.theme.secondaryText});
     filter: drop-shadow(0px 0px 0.4rem rgba(${props => props.theme.primaryText}, 0.8));
+    
+    @media(max-width: 768px) {
+      font-size: larger;
+    }
 `;
