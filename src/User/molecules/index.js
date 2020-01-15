e4caed707b29c2864093ac6ebeb41293018733c8
Toleckk <anatolich1995@gmail.com>
2020-01-15 04:27:29 +0300

    Index fixes

--- src/User/molecules/index.js
+++ src/User/molecules/index.js
@@ -1,5 +1,7 @@
 import Button from "./Button";
 import UserStack from "./UserStack";
 import UsersIntersection from "./UsersIntersection";
+import Description from "./Description";
+import AdditionalDescription from "./AdditionalDescription";
 
-export {Button, UserStack, UsersIntersection};
+export {Button, UserStack, UsersIntersection, Description, AdditionalDescription};
