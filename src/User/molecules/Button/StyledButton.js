16af312c6ec0ddb769f8a82351b7cf9ff76a951e
Toleckk <anatolich1995@gmail.com>
2020-01-18 00:39:16 +0300

    Flex component injected

--- src/User/molecules/Button/StyledButton.js
+++ src/User/molecules/Button/StyledButton.js
@@ -1,24 +1,25 @@
 import styled from "styled-components";
 import Type from "prop-types";
+import {Flex} from "Common/atoms";
 
 const sizes = {
     small: 30,
     big: 50,
 };
 
-const StyledButton = styled.button`
-    border-radius: 50%;
-    display: flex;
-    
-    width: ${props => props.size === 'full' ? '100%' : (sizes[props.size] + 'px')}
-    height: ${props => props.size === 'full' ? '100%' : (sizes[props.size] + 'px')}
+const StyledButton = styled(Flex).attrs({as: 'button'})`
+    width: ${props => props.size === 'full' ? '100%' : (sizes[props.size] + 'px')};
+    height: ${props => props.size === 'full' ? '100%' : (sizes[props.size] + 'px')};
+    ${props => props.type === 'circle' && 'border-radius: 50%;'}
 `;
 
 StyledButton.defaultProps = {
+    type: 'circle',
     size: 'small',
 };
 
 StyledButton.propTypes = {
+    type: Type.oneOf(['circle', 'usual']),
     size: Type.oneOfType([Type.oneOf(['small', 'big', 'full']), Type.string]),
 };
 
