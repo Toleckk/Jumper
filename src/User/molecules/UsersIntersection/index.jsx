2650caf63b78bfb15abec16ccbb32f7f1f7c46c8
Toleckk <anatolich1995@gmail.com>
2020-01-18 00:42:27 +0300

    Translation injected

--- src/User/molecules/UsersIntersection/index.jsx
+++ src/User/molecules/UsersIntersection/index.jsx
@@ -2,16 +2,18 @@ import React, {useContext} from 'react';
 import Type from "prop-types";
 import {ThemeContext} from "styled-components";
 import {Link} from "Common/atoms";
+import {useTranslation} from "Common/contexts/Localization"
 import {Avatar, Icon} from "../../atoms";
 import Container from "./Container";
 import {UserStack} from "../index";
 
 const UsersIntersection = ({users, count, ...props}) => {
     const {primaryText} = useContext(ThemeContext);
+    const {t} = useTranslation();
 
     if (count === 1 && users.length)
         return <Container as={Link} to={`/@${users[0].nickname}`} {...props}>
-            Читает&nbsp;
+            {t('Reads')}&nbsp;
             <Avatar size="small" src={users[0].avatar}/>
             &nbsp;
             {users[0].nickname}
@@ -19,17 +21,17 @@ const UsersIntersection = ({users, count, ...props}) => {
 
     if (count > 1 && count <= 3)
         return <Container {...props}>
-            Читают&nbsp;
+            {t('Read')}&nbsp;
             <UserStack users={users}/>
             &nbsp;
         </Container>;
 
     if (count > 3)
         return <Container {...props}>
-            Читают&nbsp;
+            {t('Read')}&nbsp;
             <UserStack users={users}/>
             &nbsp;
-            и ещё {count - 3}
+            {t('and another') + ' ' + (count - 3)}
             &nbsp;
             <Icon icon="user" size="1em" color={`rgb(${primaryText})`}/>
         </Container>;
