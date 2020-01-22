7fbd6bd5e76b14be7502426232c788ce0cce4f1c
Toleckk <anatolich1995@gmail.com>
2020-01-23 00:19:08 +0300

    Styles fixes

--- src/Common/atoms/GlobalAppContainer.js
+++ src/Common/atoms/GlobalAppContainer.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export default styled.div`
-    min-width: 100vw;
     min-height: 100vh;
     background: rgb(${props => props.theme.primaryDark});
     color: rgb(${props => props.theme.primaryText});
