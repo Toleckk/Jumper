4a93d005d71b8112ad88fb3bef573e131f745b48
Toleckk <anatolich1995@gmail.com>
2020-01-23 01:45:53 +0300

    Temp border removed

--- src/User/organisms/UserCard/Container.js
+++ src/User/organisms/UserCard/Container.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import {Flex} from "Common/atoms";
 
-// TODO: change color
 export default styled(Flex).attrs({direction: 'column'})`
     width: 55vw;
-    border: 1px solid white;
     padding: 10px;
     box-sizing: border-box;
     position: relative;
