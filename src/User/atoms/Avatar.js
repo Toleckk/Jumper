ab907fa007c6b121637d231c695af56dc11b6914
Toleckk <anatolich1995@gmail.com>
2020-01-23 19:34:56 +0300

    Avatar refactored

--- src/User/atoms/Avatar.js
+++ src/User/atoms/Avatar.js
@@ -3,39 +3,75 @@ import Type from "prop-types";
 
 const sizes = {
     big: {
-        size: '150px',
-        mobile: '100px'
+        desktop: {
+            size: '150px',
+            borderSize: '4px',
+            shadow: '25px',
+        },
+        mobile: {
+            size: '100px',
+            borderSize: '2px',
+            shadow: '20px',
+        },
+        offset: '-30px',
+
     },
     small: {
-        size: '30px',
+        desktop: {
+            size: '30px',
+            borderSize: '1px',
+            shadow: '1px',
+        },
+        mobile: {
+            size: '30px',
+            borderSize: '1px',
+            shadow: '1px',
+        },
         offset: '-12px',
     },
+    header: {
+        desktop: {
+            size: '40px',
+            borderSize: '1px',
+            shadow: '1px',
+        },
+        mobile: {
+            size: '40px',
+            borderSize: '1px',
+            shadow: '1px',
+        },
+        offset: '-12px',
+    }
 };
 
 const Avatar = styled.img`
-    user-select: none;
-    cursor: pointer;
-    border-radius: 50%;
-    width: ${props => sizes[props.size].size};
-    height: ${props => sizes[props.size].size};
-    
-    @media(max-width: 768px) {
-      width: ${props => sizes[props.size].mobile || sizes[props.size].size};
-      height: ${props => sizes[props.size].mobile || sizes[props.size].size};
-      ${props => props.border && `border: ${props.size === 'big' ? 2 : 1}px solid rgb(${props.theme.primary});
-                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 20 : 1}px;
-      `}
-    }
-    
-    ${props => props.stacked && `margin-left: ${sizes[props.size].offset}`}
-    ${props => props.border && `border: ${props.size === 'big' ? 4 : 1}px solid rgb(${props.theme.primary});
-                                box-shadow: rgb(${props.theme.primary}) 0 0 ${props.size === 'big' ? 25 : 1}px;
-    `}
-    background: rgb(${props => props.theme.primaryDark});
-    
-    ${props => props.hover && `&:hover {
+  user-select: none;
+  cursor: pointer;
+  border-radius: 50%;
+  
+  background: rgb(${props => props.theme.primaryDark});
+  
+  ${props => props.hover && `&:hover {
       filter: brightness(0.8);
-    }`}
+    }`
+  }
+  
+  ${props => props.stacked && `margin-left: ${sizes[props.size].offset}`}
+  
+  
+  ${props => createStyles(props, sizes[props.size].mobile)};
+  
+  @media (min-width: 768px) {
+    ${props => createStyles(props, sizes[props.size].desktop)};
+  }
+`;
+
+const createStyles = ({theme, border}, {size, borderSize, shadow}) => `
+    width: ${size};
+    height: ${size};
+        
+    ${border ? `border: ${borderSize} solid rgb(${theme.primary});` : ''}
+    ${border ? `box-shadow: rgb(${theme.primary}) 0 0 ${shadow};` : ''}
 `;
 
 Avatar.defaultProps = {
@@ -46,7 +82,7 @@ Avatar.defaultProps = {
 };
 
 Avatar.propTypes = {
-    size: Type.oneOf(['big', 'small']),
+    size: Type.oneOf(['big', 'small', 'header']),
     border: Type.bool,
     stacked: Type.bool,
     hover: Type.bool,
