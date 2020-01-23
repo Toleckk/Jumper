3ae5f8e69a04ba8b3cd5e9882f96535c001e7bdf
Toleckk <anatolich1995@gmail.com>
2020-01-23 23:54:08 +0300

    Semantic tag added

--- src/User/organisms/UserCard/Container.js
+++ src/User/organisms/UserCard/Container.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import {Flex} from "Common/atoms";
 
-export default styled(Flex).attrs({direction: 'column'})`
+export default styled(Flex).attrs({direction: 'column', as: 'section'})`
     padding: 10px;
     box-sizing: border-box;
     position: relative;
