aa1cbcee3a658461f603865a2d9c4c1ce95bc006
Toleckk <anatolich1995@gmail.com>
2020-01-23 23:04:43 +0300

    Styles fixes

--- src/User/organisms/UserCard/Container.js
+++ src/User/organisms/UserCard/Container.js
@@ -2,12 +2,7 @@ import styled from "styled-components";
 import {Flex} from "Common/atoms";
 
 export default styled(Flex).attrs({direction: 'column'})`
-    width: 55vw;
     padding: 10px;
     box-sizing: border-box;
     position: relative;
-    
-    @media(max-width: 768px){
-        width: 100vw;
-    }
 `;
