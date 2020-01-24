9b2e211a325cc93b4fd6f67098e5f9282290c2a5
Toleckk <anatolich1995@gmail.com>
2020-01-24 15:36:37 +0300

    Sizes for nickname added

--- src/User/atoms/Nickname.js
+++ src/User/atoms/Nickname.js
@@ -1,13 +1,24 @@
 import styled from "styled-components";
+import Type from "prop-types";
 
-export default styled.span`
-    font-size: x-large;
+const Nickname = styled.span`
+    font-size: ${props => props.size === 'big' ? 'x-large' : 'large'};
     font-weight: 700;
     line-height: 2.25;
     color: rgb(${props => props.theme.secondaryText});
     filter: drop-shadow(0px 0px 0.4rem rgba(${props => props.theme.primaryText}, 0.8));
     
     @media(max-width: 768px) {
-      font-size: larger;
+      font-size: ${props => props.size === 'big' ? 'larger' : 'medium'};
     }
 `;
+
+Nickname.defaultProps = {
+    size: 'big',
+};
+
+Nickname.propTypes = {
+    size: Type.oneOf(['big', 'small']),
+};
+
+export default Nickname;
