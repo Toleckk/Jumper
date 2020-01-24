dc890ac91fff8b687e1887db4bf2e096a1880bc6
Toleckk <anatolich1995@gmail.com>
2020-01-25 00:10:34 +0300

    Cutting text added

--- src/User/atoms/Nickname.js
+++ src/User/atoms/Nickname.js
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Type from "prop-types";
 
-const Nickname = styled.span`
+const Nickname = styled.div`
     font-size: ${props => props.size === 'big' ? 'x-large' : 'large'};
     font-weight: 700;
     line-height: 2.25;
     color: rgb(${props => props.theme.secondaryText});
     filter: drop-shadow(0px 0px 0.4rem rgba(${props => props.theme.primaryText}, 0.8));
+    
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
     
     @media(max-width: 768px) {
       font-size: ${props => props.size === 'big' ? 'larger' : 'medium'};
