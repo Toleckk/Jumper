6bd375d75d0e9862461523008a994d4637ea2665
heiNina <nina.shabrova2001@gmail.com>
2020-05-28 23:52:46 +0300

    Added: authorization for privacy

--- src/Common/apollo/entities/post.js
+++ src/Common/apollo/entities/post.js
@@ -1,22 +1,5 @@
 import gql from 'graphql-tag'
 
-export const POSTS_CONNECTION = gql`
-    query PostsConnection($owner: String!, $after: String, $first: Int!) {
-        postsConnection(owner: $owner, after: $after, first: $first) {
-            edges {
-                node {
-                    text
-                }
-            }
-            pageInfo {
-                hasNextPage
-                hasPreviousPage
-                endCursor
-            }
-        }
-    }
-`;
-
 export const CREATE_POST = gql`
     mutation CreatePost($text: String!) {
         createPost(post: {text: $text})
