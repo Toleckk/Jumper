3d6a407417f2f3ab0f49067ad634e07456c4a29c
heiNina <nina.shabrova2001@gmail.com>
2020-06-01 03:20:07 +0300

    Fixed: UI

--- src/Landing/pages/Login/components/AuthorizationForm/StyledButton.js
+++ src/Landing/pages/Login/components/AuthorizationForm/StyledButton.js
@@ -4,4 +4,10 @@ import {Button} from "Common/atoms";
 export default styled(Button)`
     margin-top: 30px;
     width: 55%;
+    
+    @media (max-width: 768px) {
+      width: auto;
+      padding-right: 2rem;
+      padding-left: 2rem;
+    }
 `;
