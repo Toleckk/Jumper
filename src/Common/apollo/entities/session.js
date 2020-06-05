76aea11959a685c852d5d5ff9f20d357ea1768fd
Toleckk <anatolich1995@gmail.com>
2020-06-05 09:28:24 +0300

    Added: feed title

--- src/Common/apollo/entities/session.js
+++ src/Common/apollo/entities/session.js
@@ -4,10 +4,10 @@ export const CREATE_SESSION = gql`
     mutation CreateSession($login: String!, $password: String!) {
         createSession(credentials: {login: $login, password: $password})
     }
-`;
+`
 
 export const LOGOUT = gql`
     mutation Logout {
         removeSession
     }
-`;
+`
